232ebcd4482c9a8fb68b653856a981269991ea17
vikas kumar <vikaskumar6399@gmail.com>
2025-03-16 18:48:57 -0400

    debug

--- myproject/myproject/settings/production.py
+++ myproject/myproject/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True  # Temporarily enabled for debugging
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
